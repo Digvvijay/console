8fddbc521e33752bbf32e3c727bfc17a71f0a203
Digvvijay <106813558+Digvvijay@users.noreply.github.com>
2022-09-24 15:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log
+
+var mfcp= "My 1st console project";
+console.log(mfcp)
+document.getElementById.innerHTML=mfcp;
+
+var n= "RanbirKapoor";
+console.log(n)
+document.getElementById.innerHTML=n;
+
+var a= "15";
+console.log(a)
+document.getElementById.innerHTML=a;
+
+var b= "5";
+console.log(b)
+document.getElementById.innerHTML=b;
+
+var c= "50";
+console.log(c)
+document.getElementById.innerHTML=c;
+
+var d= "2";
+console.log(d)
+document.getElementById.innerHTML=d;
